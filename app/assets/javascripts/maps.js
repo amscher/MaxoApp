6708083526cb76bd24fa236b03ab433aa8559399
Montana Scher <amscher@gmail.com>
2014-04-01 16:29:19 -0700

    added to maps.js ability to see distance with nearby theaters

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,4 +1,12 @@
 var map;
+var spinner;
+var mapContainerEl;
+var infoContainerEl;
+var labels = [];
+var selectedTheater;
+
+G_ICON = "http://mt.google.com/vt/icon?psize=30&font=fonts/arialuni_t.ttf&color=ff304C13&name=icons/spotlight/spotlight-waypoint-a.png&ax=43&ay=48&text=%E2%80%A2";
+R_ICON = "https://mts.googleapis.com/vt/icon/name=icons/spotlight/spotlight-poi.png&scale=1";
 
 function initialize() {
   var opts = {
@@ -25,17 +33,16 @@ function initialize() {
     zoom: 7
   };
 
-  var infoContainerEl = document.getElementById('theater-info-container');
+  infoContainerEl = document.getElementById('theater-info-container');
   map = new google.maps.Map(document.getElementById("map-canvas"),
       mapOptions);
 
-  var mapContainerEl = document.getElementById('map-canvas');
-  var spinner = new Spinner(opts);
+  mapContainerEl = document.getElementById('map-canvas');
+  spinner = new Spinner(opts);
 
   map.data.loadGeoJson('/map.json');
   map.data.setStyle(function(feature) {
     var theaterName = feature.getProperty('name');
-    console.log(theaterName);
     return {
       visible: true,
       clickable: true,
@@ -43,16 +50,97 @@ function initialize() {
     };
   });
 
+  // Define the circle
+  var circleL = new google.maps.Circle({
+      map: map,
+      clickable: false,
+      // metres
+      radius: 16093,
+      strokeColor: '#313131',
+      fillOpacity: 0,
+      strokeOpacity: .8,
+      strokeWeight: .8
+  });
+
+  // Define the circle
+  var circleM = new google.maps.Circle({
+      map: map,
+      clickable: false,
+      // metres
+      radius: 8000,
+      strokeColor: '#313131',
+      fillOpacity: 0,
+      strokeOpacity: .8,
+      strokeWeight: .8
+  });
+
+    // Define the circle
+  var circleS = new google.maps.Circle({
+      map: map,
+      clickable: false,
+      // metres
+      radius: 1000,
+      strokeColor: '#313131',
+      fillOpacity: 0,
+      strokeOpacity: .8,
+      strokeWeight: .8
+  });
+
   // Set mouseover event for each feature.
   map.data.addListener('click', function(event) {
+    if (selectedTheater != null) map.data.overrideStyle(selectedTheater, {icon: R_ICON});
+
+    // RESET EVERYTHING
+    selectedTheater = event.feature;
+    for (var label in labels) {
+      labels[label].setMap(null);
+    }
+    labels = [];
+
+    // Start loader
     spinner.spin(mapContainerEl);
-    var theaterId = event.feature.getProperty('rentrak_id');
-    var latlng = event.feature.getGeometry().get();
+
+    var theaterId = selectedTheater.getProperty('rentrak_id');
+    var latlng = selectedTheater.getGeometry().get();
     map.panTo(latlng);
     if (map.getZoom() < 11) {
       map.setZoom(11);
     }
+    getTheaterInfo(theaterId, latlng);
 
+    // UPDATE icon for selected theater!
+    map.data.overrideStyle(selectedTheater, {icon: G_ICON});
+
+    // Attach circle to marker
+    circleL.bindTo('center', selectedTheater.getGeometry(), 'position');
+    circleM.bindTo('center', selectedTheater.getGeometry(), 'position');
+    circleS.bindTo('center', selectedTheater.getGeometry(), 'position');
+    var bounds = circleL.getBounds();
+
+    map.data.forEach(function(feature) {
+      nearLatLng = feature.getGeometry().get();
+      if (nearLatLng != latlng && bounds.contains(nearLatLng)) {
+        // Get distance, display label
+        var distance =
+            google.maps.geometry.spherical.computeDistanceBetween(nearLatLng, latlng);
+        distance = (distance * 0.000621371).toFixed(2);
+
+        var marker = new MarkerWithLabel({
+           position: nearLatLng,
+           map: map,
+           labelContent: feature.getProperty("name") + " (" + distance + "mi)",
+           labelAnchor: new google.maps.Point(50, 0),
+           labelClass: "distance-labels", // the CSS class for the label
+           icon: {}
+         });
+
+        labels.push(marker);
+      }
+    });
+  });
+}
+
+function getTheaterInfo(theaterId, latlng) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', '/map/theater_info/' + theaterId, true);
     xhr.onreadystatechange = function() {
@@ -64,10 +152,10 @@ function initialize() {
         infoContainerEl.style.visibility = 'visible';
         infoContainerEl.innerHTML = this.responseText;
 
-        var mapHeight = window.innerHeight - infoContainerEl.clientHeight;
-        mapContainerEl.style.height = mapHeight + "px";
-        google.maps.event.trigger(map, 'resize');
-        map.panTo(latlng);
+        // var mapHeight = window.innerHeight - infoContainerEl.clientHeight;
+        // mapContainerEl.style.height = mapHeight + "px";
+        // google.maps.event.trigger(map, 'resize');
+        // map.panTo(latlng);
 
         return;
       } else {
@@ -75,7 +163,6 @@ function initialize() {
       }
     };
     xhr.send();
-  });
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
